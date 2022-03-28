503a60bfc10b5e0f6d4c638eb8d022e8de7e319d
anasonov <agnason@gmail.com>
2022-03-28 19:14:18 +0300

    lesson04_final

--- server/src/main/java/server/StartServer.java
+++ server/src/main/java/server/StartServer.java
@@ -1,8 +1,14 @@
 package server;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class StartServer {
     public static void main(String[] args) {
-        new Server();
-
+        // lesson04 //
+        ExecutorService service= Executors.newFixedThreadPool(4);
+        service.execute(() -> new Server());
+        service.shutdown();
+        // lesson04 //
     }
 }
